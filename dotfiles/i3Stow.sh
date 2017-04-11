fb5f4a207f76627423e7bd5e2410a4fff3ddc27d
pierregermain <pierre2008@gmail.com>
2017-04-11 01:01:06 -0500

    dependencies for dmenu extended

--- dotfiles/i3Stow.sh
+++ dotfiles/i3Stow.sh
@@ -1,6 +1,8 @@
 #debian/ubuntu
 sudo apt-get update
 sudo apt install suckless-tools
+sudo apt install xfonts-terminus
 sudo apt install conky
+echo "See install of dmenu_extended at https://github.com/markjones112358/dmenu-extended"
 #configs
 stow -v -t ~ i3
